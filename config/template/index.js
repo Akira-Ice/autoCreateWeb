8446a17974b28f205c57ea074e87677e149d6ce9
amos <amoslier@163.com>
2023-06-30 18:57:55 +0800

    👌 模板配置文件模块化

---
+++ config/template/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: AkiraIce
+ * @Date: 2023-06-29 13:26:36
+ * @LastEditTime: 2023-06-30 16:13:34
+ * @LastEditors: AkiraIce
+ * @Description: home 组件模板
+ */
+const homeTemp = require("./template_homeTemp");
+const manageTemp = require("./template_manageTemp");
+const newsTemp = require("./template_newsTemp");
+
+module.exports = { homeTemp, newsTemp, manageTemp };
