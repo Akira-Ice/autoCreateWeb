07d7d4ee75ec70322a6f84dc3639e4375236961c
amos <amoslier@163.com>
2023-06-30 18:58:59 +0800

    🍻 加点注释

--- autoCreate_view.js
+++ autoCreate_view.js
@@ -1,28 +1,46 @@
 /*
  * @Author: Akira
  * @Date: 2023-04-20 18:44:12
- * @LastEditTime: 2023-06-28 16:53:33
+ * @LastEditTime: 2023-06-30 16:14:13
  */
 const fs = require("fs");
 const menus = require("./config/menus");
 const deleteFolder = require("./utils/deleteFolder");
+const { homeTemp, newsTemp, manageTemp } = require("./config/template");
 
+function getTemp(menuName) {
+  return menuName == "home" ? homeTemp : menuName == "news" ? newsTemp : manageTemp;
+}
+
+/**
+ * 创建view目录以及组件文件
+ * @param {object} menus
+ */
 async function createView(menus) {
   try {
+    /** 模板 */
+    let temp = "";
+    /** 创建view目录 */
     await fs.promises.mkdir("dist/view", { recursive: true });
+
+    /** 遍历菜单 */
     menus.map(async (menu) => {
+      /** 获取菜单 id */
       const menuName = menu.id.substring(1);
+      /** 创建 vue 文件 */
       await fs.promises.mkdir(`dist/view/${menuName}`, { recursive: true });
+
+      /** 子菜单 */
       if (menu.children) {
         menu.children.map(async (item) => {
           const itemName = item.id.substring(1);
-          await fs.promises.writeFile(
-            `dist/view/${menuName}/${itemName}.vue`,
-            ""
-          );
+          temp = getTemp(itemName);
+          /** 写入组件文件 */
+          await fs.promises.writeFile(`dist/view/${menuName}/${itemName}.vue`, temp);
         });
       } else {
-        await fs.promises.writeFile(`dist/view/${menuName}/index.vue`, "");
+        temp = getTemp(menuName);
+        await fs.promises.writeFile(`dist/view/${menuName}/index.vue`, temp);
       }
     });
     console.log("File has been created and saved!");
